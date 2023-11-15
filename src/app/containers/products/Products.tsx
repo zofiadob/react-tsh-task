fea58dbe81c57421305e7a2a99bc2dfff6da3218
zofiadob <z.dobrowolska24@gmail.com>
2023-11-15 20:28:21 +0100

    feat: add chakra ui

--- src/app/containers/products/Products.tsx
+++ src/app/containers/products/Products.tsx
@@ -1,3 +1,6 @@
+import { Text } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+
 import { AppLocale } from 'context/locale/AppLocale.enum';
 import { useLocale } from 'hooks/useLocale/useLocale';
 import { Translation } from 'ui/translation/Translation';
@@ -6,23 +9,26 @@ export const Products = () => {
   const { locale, setLocale } = useLocale();
 
   return (
-    <>
-      <h2>Home</h2>
-      <p>
-        <Translation id="home.helloWorld" />
-        <span style={{ margin: '0 1rem' }}>&#x2190;</span>
-        <span>
-          This text is translated using <strong>Translation</strong> component.
-        </span>
-        <span>Click </span>
-        <button
-          style={{ fontSize: 'inherit' }}
-          onClick={() => setLocale(locale === AppLocale.pl ? AppLocale.en : AppLocale.pl)}
-        >
-          here
-        </button>{' '}
-        to change language.
-      </p>
-    </>
+    <ChakraProvider>
+      <>
+        <h2>Home</h2>
+        <p>
+          <Text fontSize="sm">In love with React & Next</Text>
+          <Translation id="home.helloWorld" />
+          <span style={{ margin: '0 1rem' }}>&#x2190;</span>
+          <span>
+            This text is translated using <strong>Translation</strong> component.
+          </span>
+          <span>Click </span>
+          <button
+            style={{ fontSize: 'inherit' }}
+            onClick={() => setLocale(locale === AppLocale.pl ? AppLocale.en : AppLocale.pl)}
+          >
+            here
+          </button>{' '}
+          to change language.
+        </p>
+      </>
+    </ChakraProvider>
   );
 };
